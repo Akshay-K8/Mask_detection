c71746286f4c10b5b1afb74882201565fc35158f
Akshay kharate <94613795+Akshay-K8@users.noreply.github.com>
2024-10-08 18:53:09 +0530

    Applied the bounding boxes and text labels

--- app.py
+++ app.py
@@ -28,8 +28,7 @@ def process_image(image_file):
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Load the pre-trained face detection classifier
-    face_cascade = cv2.dnn.readNetFromCaffe('deploy.prototxt', 'res10_300x300_ssd_iter_140000.caffemodel')
-# Use face_net for detection instead of Haar Cascade
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     # Detect faces in the image
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10)
     
